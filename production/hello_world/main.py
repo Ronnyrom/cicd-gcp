6004355c405d8093e79006b808b59b2d57a0c838
Ronnyrom <ronny.romero@e-campus.uab.cat>
2020-05-29 20:28:19 +0200

    coma corregida

--- production/hello_world/main.py
+++ production/hello_world/main.py
@@ -18,7 +18,7 @@ import webapp2
 class MainPage(webapp2.RequestHandler):
     def get(self):
         self.response.headers['Content-Type'] = 'text/plain'
-        self.response.write('Hello, World!,this is production environment'2222)
+        self.response.write('Hello, World!,this is production environment 2222')
 
 
 app = webapp2.WSGIApplication([
